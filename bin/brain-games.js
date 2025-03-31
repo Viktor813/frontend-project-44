9211692b499391dcedf5a50ef2e7b3b285490574
Тёмочка <artem_sherstnev_2000@mail.ru>
2025-03-31 13:58:18 +0700

    Kakashka

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,32 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
 import readlineSync from 'readline-sync';
+export const brainivn = () => {
+  console.log('Welcome to the Brain Games!')
+  const name = readlineSync.question('May I have your name? ');
+  const greeting = `Hello, ${name}!`
+  console.log(greeting);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const getRamdemint = () => {
+    return Math.floor(Math.random() * 100)
+  }
+  let wins = 0
+  while (wins < 3) {
 
-const name = readlineSync.question('May I have your name? ');
-const greeting = `Hello, ${name}!`
-console.log(greeting);
+
+    const num = getRamdemint()
+    console.log(`Question: ${num}`);
+    const answer = readlineSync.question(`Your answer: `)
+    const rightAnsver = num % 2 === 0 ? 'yes' : 'no'
+    if (answer.toLowerCase() === rightAnsver) {
+      console.log(`Correct!`);
+      wins += 1
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnsver}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return 
+    }
+    if (wins === 3)
+      console.log(`Congratulations,${name}!`);
+  }
+}
+brainivn()
